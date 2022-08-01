739c26203a4c740e4f6947f10e4c87c94b30e058
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-01 16:56:43 -0400

    Implemented Clear and Delete functionality.
    Implemented Clear and Delete functionality.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
 
 let number1 = null, number2 = null;
-let calculate = false;
-let textNode = ""
-let operator = "", oldOperator = "";
+let textNode = "", operator = "", oldOperator = "";
 
 function divide(a, b) {
     return a / b;
@@ -27,16 +25,14 @@ function updateNumbers() {
 
     numberContainer.append(textNode);
 
-    if(number1 === null)
-     number1 = parseFloat(textNode);
+    if (number1 === null)
+        number1 = parseFloat(textNode);
 
-     if(number2 === null)
-     number2 = parseFloat(textNode);
+    if (number2 === null)
+        number2 = parseFloat(textNode);
 }
 
 function updateOperators() {
-
-
     if (number1 !== null && number2 !== null) {
         if (operator === "÷")
             number1 = divide(number1, number2);
@@ -46,16 +42,15 @@ function updateOperators() {
             number1 = subtract(number1, number2);
         else if (operator === "+")
             number1 = add(number1, number2);
-        else if(operator === "=")
-        {
+        else if (operator === "=") {
             if (oldOperator === "÷")
-            number1 = divide(number1, number2);
-        else if (oldOperator === "x")
-            number1 = multiply(number1, number2);
-        else if (oldOperator === "-")
-            number1 = subtract(number1, number2);
-        else if (oldOperator === "+")
-            number1 = add(number1, number2);
+                number1 = divide(number1, number2);
+            else if (oldOperator === "x")
+                number1 = multiply(number1, number2);
+            else if (oldOperator === "-")
+                number1 = subtract(number1, number2);
+            else if (oldOperator === "+")
+                number1 = add(number1, number2);
 
             oldOperator = "";
         }
@@ -77,8 +72,8 @@ function updateOperators() {
     while (operatorContainer.firstChild)
         operatorContainer.firstChild.remove();
 
-    if(this.getAttribute("id") === "=")
-    oldOperator = operator;
+    if (this.getAttribute("id") === "=")
+        oldOperator = operator;
 
     operator = this.getAttribute("id");
 
@@ -87,12 +82,51 @@ function updateOperators() {
     textNode = "";
 }
 
+function clear() {
+    const numberContainer = document.querySelector("#numberContainer");
+    const operatorContainer = document.querySelector("#operatorContainer");
+
+    while (operatorContainer.firstChild)
+        operatorContainer.firstChild.remove();
+
+    while (numberContainer.firstChild)
+        numberContainer.firstChild.remove();
+
+    oldOperator = "";
+    operator = "";
+    textNode = "";
+}
+
+function deleteFunction() {
+    const numberContainer = document.querySelector("#numberContainer");
+    const operatorContainer = document.querySelector("#operatorContainer");
+
+    while (operatorContainer.firstChild)
+        operatorContainer.firstChild.remove();
+
+    while (numberContainer.firstChild)
+        numberContainer.firstChild.remove();
+
+    oldOperator = "";
+    operator = "";
+    textNode = "";
+
+    number1 = null;
+    number2 = null;
+}
+
 function addListeners() {
     const numberButtons = document.querySelectorAll(".buttonStyle.number");
     numberButtons.forEach(button => button.addEventListener("mousedown", updateNumbers));
 
     const operatorButtons = document.querySelectorAll(".buttonStyle.operator");
     operatorButtons.forEach(button => button.addEventListener("mousedown", updateOperators));
+
+    const clearButton = document.querySelector("#clearButton");
+    clearButton.addEventListener("mousedown", clear);
+
+    const deleteButton = document.querySelector("#deleteButton");
+    deleteButton.addEventListener("mousedown", deleteFunction);
 }
 
 window.onload = addListeners();
